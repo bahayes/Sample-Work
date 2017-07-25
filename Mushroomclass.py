61a569b9616ec1d2ff188df92449c8c347eea132
bahayes <bahayes@users.noreply.github.com>
2017-07-25 12:48:55 -0700

    Update Mushroomclass.py

--- Mushroomclass.py
+++ Mushroomclass.py
@@ -1,6 +1,9 @@
 '''Brittany Hayes
    Artificial Intelligence
-   classify mushrooms as either poisonous (1) or edible (0)'''
+   classify mushrooms as either poisonous (1) or edible (0)
+   Data from https://archive.ics.uci.edu/ml/datasets/mushroom
+   (Lichman, M. (2013). UCI Machine Learning Repository [http://archive.ics.uci.edu/ml]. Irvine, CA: University of California, School of Information and Computer Science.)
+   '''
 
 import numpy as np
 import pandas as pd
